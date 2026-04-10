204686d8515f01cbe02a22e59a21f4fe25f131c5
Meoyoug <eodyd426@gmail.com>
2026-04-10 10:27:16 +0900

    ✨Feat: 요구사항 변경에 따른 모델 및 마이그레이션 수정

--- app/models/users.py
+++ app/models/users.py
@@ -4,8 +4,7 @@ from sqlalchemy import String, Enum, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.core.db.databases import Base
 from app.core.db.models import TimestampMixin
-from app.core.enums import UserRole, Gender
-
+from app.core.enums import UserRole, Gender, Department
 
 if TYPE_CHECKING:
     from app.models.medical_records import XrayImage
@@ -17,8 +16,9 @@ class User(Base, TimestampMixin):
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255))
-    nickname: Mapped[str] = mapped_column(String(10), unique=True, index=True)
-    role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.USER)
+    name: Mapped[str] = mapped_column(String(30), index=True)
+    department: Mapped[str] = mapped_column(Enum(Department))
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.PENDING)
     phone_number: Mapped[str] = mapped_column(String(11))
     gender: Mapped[str] = mapped_column(Enum(Gender))
     is_active: Mapped[bool] = mapped_column(default=True)
