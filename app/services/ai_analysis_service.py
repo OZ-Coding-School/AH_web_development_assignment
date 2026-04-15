69c7c74cf430930475f4b2fb8de184036cc26ae4
Meoyoug <eodyd426@gmail.com>
2026-04-15 16:52:38 +0900

    🎨Style: run ruff formatting

--- app/services/ai_analysis_service.py
+++ app/services/ai_analysis_service.py
@@ -10,9 +10,7 @@ from app.repositories.medical_record_repository import MedicalRecordRepository
 from ai_models.model import PneumoniaPredictModel, logger
 
 
-MODEL_PATHS = {
-    "pneumonia_model_v1": "ai_models/pneumonia_model.pth"
-}
+MODEL_PATHS = {"pneumonia_model_v1": "ai_models/pneumonia_model.pth"}
 pneumonia_model = PneumoniaPredictModel(MODEL_PATHS.get(settings.PREDICT_MODEL, ""))
 
 try:
